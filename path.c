d380eb7d2a96140b6ef39443fc34e1a98964c445
Bnobody47 <bnobodygetmoney@gmail.com>
2023-05-22 14:07:35 -0700

    new path

--- path.c
+++ path.c
@@ -16,7 +16,7 @@ int new_path(char **args)
 	{
 		if (execvp(args[0], args) == -1)
 		{
-			perror("error in new_path: child process");
+			perror("error in simple_shell:");
 		}
 		exit(EXIT_FAILURE);
 
