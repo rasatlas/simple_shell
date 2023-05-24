529d50792ba57b50add54e78bce44066282d910b
Zemichael <yetikimitabeba@gmail.com>
2023-05-25 02:16:56 +0300

    setting and unsetting environment variables included

--- _env.c
+++ _env.c
@@ -19,3 +19,127 @@ int _env(char **args)
 	}
 	return (-1);
 }
+
+/**
+ * _getenv - gets the value of the global variable
+ * @name: name of the global variable
+ * Return: string of value
+ */
+
+char *_getenv(const char *name)
+{
+	int i, j;
+	char *value;
+
+	if (!name)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (name[j] == environ[i][j])
+		{
+			while (name[j])
+			{
+				if (name[j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (name[j] == '\0')
+			{
+				value = (environ[i] + j + 1);
+				return (value);
+			}
+		}
+	}
+	return (0);
+}
+
+/**
+ * _setenv - Initialize a new environment variable, or modify an existing one
+ * @args: array of entered words
+ */
+
+void _setenv(char **args)
+{
+	int i, j, k;
+
+	if (!args[1] || !args[2])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (args[1][j] == environ[i][j])
+		{
+			while (args[1][j])
+			{
+				if (args[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (args[1][j] == '\0')
+			{
+				k = 0;
+				while (args[2][k])
+				{
+					environ[i][j + 1 + k] = args[2][k];
+					k++;
+				}
+				environ[i][j + 1 + k] = '\0';
+				return;
+			}
+		}
+	}
+	if (!environ[i])
+	{
+
+		environ[i] = concat_all(args[1], "=", args[2]);
+		environ[i + 1] = '\0';
+
+	}
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @args: array of entered words
+ */
+void _unsetenv(char **args)
+{
+	int i, j;
+
+	if (!args[1])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (args[1][j] == environ[i][j])
+		{
+			while (args[1][j])
+			{
+				if (args[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (args[1][j] == '\0')
+			{
+				free(environ[i]);
+				environ[i] = environ[i + 1];
+				while (environ[i])
+				{
+					environ[i] = environ[i + 1];
+					i++;
+				}
+				return;
+			}
+		}
+	}
+}
