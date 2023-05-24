2464346dcc15684c94458cadf2329ac00e7ddca8
Bnobody47 <bnobodygetmoney@gmail.com>
2023-05-24 15:08:45 -0700

    Change DIR Update

--- _cd.c
+++ _cd.c
@@ -8,16 +8,29 @@
 
 int _cd(char **args)
 {
-	if (args[1] == NULL)
+	char *new_dir;
+
+	if (args[1] == NULL || strcmp(args[1], "~") == 0)
 	{
-		fprintf(stderr, "Expected argument to \"cd\"\n");
+		new_dir = getenv("HOME");
+	}
+	else if (strcmp(args[1], "-") == 0)
+	{
+		new_dir = getenv("OLDPWD");
 	}
 	else
 	{
-		if (chdir(args[1]) != 0)
-		{
-			perror("Error in _cd: changing dir\n");
-		}
+		new_dir = args[1];
 	}
-	return (-1);
+
+	if (chdir(new_dir) != 0)
+	{
+		perror("cd");
+		return (0);
+	}
+
+	setenv("OLDPWD", getenv("PWD"), 1);
+	setenv("PWD", getcwd(NULL, 0), 1);
+
+	return (1);
 }
