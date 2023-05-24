a52b3c9ac76e4286785b7dcb0fc14164c9f37fd4
Zemichael <yetikimitabeba@gmail.com>
2023-05-24 23:55:22 +0300

    betty and typo edits

--- simpleShell.h
+++ simpleShell.h
@@ -14,23 +14,14 @@ extern char **environ;
 
 /* PROTOTYPES */
 
-/*---main.c---*/
 void interactive(void);
 void noninteractive(void);
-
-/*---shell_interactive---*/
 char *_getline(void);
 char **tokenizer(char *line);
 int execute_args(char **args);
-
-/*---shell_non_interactive---*/
 char *_getstream(void);
-
-/*---execute_args---*/
 int new_path(char **args);
 int num_builtins(void);
-
-/*---builtin---*/
 int _cd(char **args);
 int custom_exit(char **args);
 int _env(char **args);
